7a259cfa2558e741853d2aacf43360bcc4c44177
kaifkh20 <kaifkhan.biz@gmail.com>
2024-05-24 21:22:45 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -12,13 +13,15 @@ import java.util.Scanner;
 
 class ThreadClient extends Thread{
   private final Socket clientSocket;
-  public ThreadClient(Socket clSocket){
+  private final String dirName;
+  public ThreadClient(Socket clSocket,String dString){
     this.clientSocket = clSocket;
+    this.dirName = dString;
   }
   public void run(){
     try{
       InputStream is = clientSocket.getInputStream();
-      String message = Main.performOperation(is);
+      String message = Main.performOperation(is,dirName);
 
       OutputStream os = clientSocket.getOutputStream();
       os.write(message.getBytes());
@@ -33,7 +36,7 @@ class ThreadClient extends Thread{
 
 public class Main {
   
-  static String performOperation(InputStream is) throws IOException{
+  static String performOperation(InputStream is,String dirName) throws IOException{
 
       BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
@@ -73,6 +76,23 @@ public class Main {
         // System.out.println(word);
         message = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+ word.length() +"\r\n\r\n"+word;
       }
+      else if(path.startsWith("/files")){
+        String word = path.split("/")[2];
+        try{
+          File file = new File(dirName+"/"+word);
+          Scanner sc = new Scanner(file);
+          String content = "";
+          if(file.exists()){
+            while(sc.hasNextLine()){
+              content+=sc.nextLine();
+            }
+            sc.close();
+            message = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: "+ content.length() +"\r\n\r\n"+content;
+        }}catch(IOException e){
+          System.out.println("ERROR :"+e);
+          message = "HTTP/1.1 404 Not Found\r\n\r\n";
+        }  
+      }
       else {
         message = "HTTP/1.1 404 Not Found\r\n\r\n";
       }
@@ -94,9 +114,16 @@ public class Main {
       try {
         serverSocket = new ServerSocket(4221);
         serverSocket.setReuseAddress(true);
+      
+        String dirName="";
+      
+        if(args.length>0){
+          dirName = args[1];
+        }
+
         while (true) {
           clientSocket = serverSocket.accept(); // Wait for connection from client.
-          ThreadClient th = new ThreadClient(clientSocket);
+          ThreadClient th = new ThreadClient(clientSocket,dirName);
           th.start();
           System.out.println("accepted new connection");
 
