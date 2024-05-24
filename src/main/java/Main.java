e1a9e3b47cee39a0d7c48343aa5cafa3d8b029e1
kaifkh20 <kaifkhan.biz@gmail.com>
2024-05-24 20:35:57 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,8 @@ import java.lang.reflect.Array;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
@@ -27,49 +29,60 @@ public class Main {
       InputStream is = clientSocket.getInputStream();
       BufferedReader br = new BufferedReader(new InputStreamReader(is));
       
-      OutputStream os = clientSocket.getOutputStream();
-      ArrayList<String[]> list= new ArrayList<>();
+      // ArrayList<String[]> list = new ArrayList<>();
 
-      // String str = "
+      // String firstLine = br.readLine();
 
-      while(true){
-        String line = br.readLine();
-        if (line==null){
-          break;
-        }
-        String []request = line.split(" ",0);
-        list.add(request);
+      // String path = firstLine.split(" ")[1];
+
+      String line;
+      HashMap<String,String> values = new HashMap<>();
+
+      while((line = br.readLine()) != null && !line.isEmpty()){
+        // System.err.println(line);
+
+        String[] l = line.split(" ");
+        values.put(l[0], l[1]);
+        // sc.next();
       }
 
+      for (String key:values.keySet()){
+        System.out.println(key+" "+values.get(key));
+      }
+      // System.out.println("reaching out of loop");
+      // System.err.println(");
       // for(String[] x:list){
       //   for (String str : x) {
       //     System.out.print(str+" ");
       //   }System.out.println();
       // }
-      // System.out.println(line);;
+      // System.out.println();;
 
       String message = "";
+
+      String path = values.get("GET");
+      System.out.println(path);
       // System.out.println(request[1]);
-      if (list.get(0)[1].equals("/")) {
+      if (path.equals("/")) {
         message = "HTTP/1.1 200 OK\r\n\r\n";
-      } else if(list.get(0)[1].contains("/echo")){
+      } else if(path.startsWith("/echo")){
           // System.err.println("reaching here");
-          String word = list.get(0)[1].split("/")[2];
+          String word = path.split("/")[2];
           // System.out.println(word);
           message = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+ word.length() +"\r\n\r\n"+word;
       }
-      else if(list.get(0)[1].equals("/user-agent")){
-        String word = list.get(2)[1];
-        // System.out.println("reching here");
+      else if(path.startsWith("/user-agent")){
+        String word = values.get("User-Agent:");
+        // System.out.println("reching here user agent");
         // System.out.println(word);
         message = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+ word.length() +"\r\n\r\n"+word;
       }
       else {
         message = "HTTP/1.1 404 Not Found\r\n\r\n";
       }
-      
+      OutputStream os = clientSocket.getOutputStream();
       os.write(message.getBytes());
-      System.out.println("Response sent");
+      // System.out.println("Response sent"+message);
       os.flush();
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
